f35de80e286ded14b769074501a17f554bae3c36
ShishkinAndrey <shishkin1994@yandex.ru>
2021-02-19 20:45:33 +0300

    New types of Class Library and new class SqliteDb

--- Class_Library.py
+++ Class_Library.py
@@ -1,25 +1,44 @@
-from SQL_script import create_connection, create_table, insert_in_table, read_table
 from Library import get_library_from_json
 import sqlite3
 
 
-class Library():
-    def __init__(self, database_name: str):
-        self.database_name = database_name
-        self.connection = self.create_connection(self.database_name)
+class Library:
+    DEFAULT_DB_NAME = "library.sqlite"
+
+    def __init__(self):
+        self.__driver = SqliteDb(self.DEFAULT_DB_NAME)
+
         self.command_create_table = """
         CREATE TABLE IF NOT EXISTS Library (
           number integer primary key AUTOINCREMENT ,
-          name  ,
+          name ,
           year ,
           pages ,
-          rating,
+          rating ,
           price ,
-          author
+          author 
         );"""
         self.command_insert = "INSERT INTO Library(name,year,pages,rating,price,author) VALUES (?, ?, ?, ?, ?, ?)"
-        self.default_preview = '*'
-        self.command_read = f"SELECT {self.default_preview} FROM Library"
+        self.command_read = f"SELECT * FROM Library"
+        self.command_delete = f"DELETE from Library where number = ?"
+
+    def create_library_table(self):
+        self.__driver.execute(self.command_create_table)
+
+    def insert_book(self, values):
+        self.__driver.executemany(self.command_insert, values)
+
+    def review_all_table(self):
+        return self.__driver.fetchall(self.command_read)
+
+    def get_field(self, query, field):
+        ...
+
+
+class SqliteDb:
+    def __init__(self, database_name: str):
+        self.database_name = database_name
+        self.connection = self.create_connection(self.database_name)
 
     @staticmethod
     def create_connection(path):
@@ -31,54 +50,61 @@ class Library():
             print(f"The error '{e}' occurred")
         return connection
 
-    def create_table(self):
-        connect = self.connection
-        cursor = connect.cursor()
+    def __get_cursor(self):
+        cursor = self.connection.cursor()
+        return cursor
+
+    def __commit_and_close(self):
+        self.connection.commit()
+        self.__get_cursor().close()
+
+    def execute(self, query):
         try:
-            cursor.execute(self.command_create_table)
-            connect.commit()
-            print('Table is created successfully')
+            self.__get_cursor().execute(query)
+            self.__commit_and_close()
+            print('Query successfully')
         except sqlite3.Error as e:
             print(f"The error '{e}' occurred")
 
-    def insert_in_table(self, values):
-        connect = self.connection
-        cursor = connect.cursor()
+    def executemany(self, query, values=None):
         try:
-            cursor.executemany(self.command_insert, values)
-            connect.commit()
-            print('Table is created successfully')
+            self.__get_cursor().executemany(query, values)
+            self.__commit_and_close()
+            print('Query successfully')
         except sqlite3.Error as e:
             print(f"The error '{e}' occurred")
 
-    def review(self):
-        connect = self.connection
-        cursor = connect.cursor()
+    def fetchall(self, query):
+        cursor = self.__get_cursor()
         try:
-            cursor.execute(self.command_read)
+            cursor.execute(query)
             result = cursor.fetchall()
+            self.__commit_and_close()
             return result
         except sqlite3.Error as e:
             print(f"The error '{e}' occurred")
 
-    def add(self, book: list):
-        self.insert_in_table(book)
+    #
+    # def delete_row(self, id_row):
+    #     connect = self.connection
+    #     cursor = connect.cursor()
+    #     try:
+    #         cursor.execute(self.command_delete, (id_row, ))
+    #         connect.commit()
+    #         cursor.close()
+    #         print('Row is deleted successfully')
+    #     except sqlite3.Error as e:
+    #         print(f"The error '{e}' occurred")
 
 if __name__ == '__main__':
+    lib = Library()
+    # lib.create_library_table()
 
-    # l = Library('library.sqlite')
-    # l.create_table()
-    # column = input('Введите параметры')
-    # COMMAND_TO_READ = f"SELECT {column} FROM Library"
-    # print(l.review(COMMAND_TO_READ))
+    # lib_from_json = get_library_from_json()
+    # ll = []
+    # for i in lib_from_json:
+    #     ll.append(tuple(i.values()))
 
-    lib = get_library_from_json()
-    ll = []
-    for i in lib:
-        ll.append(tuple(i.values()))
-    print(ll)
-    # l.insert_in_table(ll)
-
-    # result = read_table(connection)
-    # for i in result:
-    #     print(i)
+    # lib.insert_book(lib.command_insert, ll)
+    # l.delete_row('3')
+    print(lib.review_all_table())
